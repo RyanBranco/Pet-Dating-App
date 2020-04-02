534539b541ae219b9452268f2faa674cd351360b
Ryan Branco <ryanbr23@gmail.com>
2020-04-01 21:00:08 -0700

    upload files to s3

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,10 @@
 const Post = require('../models/post');
-const User = require('../models/user')
+const User = require('../models/user');
+const multer = require("multer");
+const AWS = require("aws-sdk");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 module.exports = {
     newPost,
     addPost,
@@ -14,11 +19,33 @@ function newPost(req, res) {
 }
 
 function addPost(req, res) {
-    const post = new Post(req.body);
-    post.save((err, post) => {
-        if (err) return res.redirect('/post')
-        res.redirect('/matcher')
-    })
+    const file = req.file;
+
+    let s3bucket = new AWS.S3({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION
+    });
+    
+    var params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: file.originalname,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+        ACL: "public-read"
+    };
+    
+    s3bucket.upload(params, function(err, data) {
+        if (err) {
+            res.status(500).json({ error: true, Message: err });
+        } else {
+            const post = new Post(req.body);
+            post.save((err, post) => {
+                if (err) return res.redirect('/post')
+                res.redirect('/matcher')
+            })
+        }
+    });
 }
 
 function viewPost(req, res) {
--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,15 @@
 var express = require("express");
 var router = express.Router();
 var postCtrl = require('../controllers/post');
+const multer = require("multer");
+const AWS = require("aws-sdk");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.get('/', postCtrl.newPost);
 router.get('/view/:id', postCtrl.viewPost)
 
-router.post('/add', postCtrl.addPost);
+router.post('/add', upload.single("attatchment"), postCtrl.addPost);
 router.post('/view/:id/comment', postCtrl.comment)
 
 module.exports = router;
